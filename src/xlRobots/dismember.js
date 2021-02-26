f7d957a61ffded0577494a563d138c2a942e185f
Cesar <cesardmn@gmail.com>
2021-02-26 07:59:44 -0300

    implement dynamic col name

--- src/xlRobots/dismember.js
+++ src/xlRobots/dismember.js
@@ -1,38 +1,38 @@
-export default function dismember(items) {
+export default function dismember(items, colName) {
   /*
     input: list
     output: list
-    todos: [dynamic unique param, validations (types)]
+    todos: [validations, types]
   */
 
-  const uniques = __getUniques(items)
-  const dismemberedMultiple = __getDismemberMultiples(items)
+  const uniques = __getUniques(items, colName)
+  const dismemberedMultiple = __getDismemberMultiples(items, colName)
   return [...uniques, ...dismemberedMultiple]
 }
 
-function __getUniques(items) {
-  return items.filter((item) => item.QTDE === 1)
+function __getUniques(items, colName) {
+  return items.filter((item) => item[colName] === 1)
 }
 
-function __getDismemberMultiples(items) {
-  const multiples = items.filter((item) => item.QTDE > 1)
+function __getDismemberMultiples(items, colName) {
+  const multiples = items.filter((item) => item[colName] > 1)
 
   const dismemberedItems = []
 
   for (let item of multiples) {
-    dismemberedItems.push(...__dismemberItem(item))
+    dismemberedItems.push(...__dismemberItem(item, colName))
   }
 
   return dismemberedItems
 }
 
-function __dismemberItem(item) {
+function __dismemberItem(item, colName) {
   const items = []
 
-  const range = item.QTDE
+  const range = item[colName]
 
   for (let i = 0; i < range; i++) {
-    item.QTDE = 1
+    item[colName] = 1
     items.push(item)
   }
 
