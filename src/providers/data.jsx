9777456407406024aba45990a5d80ba7c09c816e
Cesar <cesardmn@gmail.com>
2021-03-01 07:53:01 -0300

    create data provider

---
+++ src/providers/data.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from 'react'
+
+export const DataContext = createContext({})
+
+export const DataProvider = (props) => {
+  const [data, setData] = useState()
+
+  return (
+    <DataContext.Provider value={{ data, setData }}>
+      {props.children}
+    </DataContext.Provider>
+  )
+}
+
+export const useData = () => useContext(DataContext)
