5571e248a3cbff7fae2e144950224c6ecb29a8bc
Cesar <cesardmn@gmail.com>
2021-03-02 11:21:28 -0300

    add accept files and json converter

--- src/xlRobots/reader.js
+++ src/xlRobots/reader.js
@@ -1,6 +1,6 @@
 import * as XLSX from 'xlsx'
 
-export async function read(file) {
+export default async function read(file) {
   try {
     return await __readFileAsync(file)
   } catch (err) {
@@ -19,10 +19,32 @@ function __readFileAsync(file) {
       const wsname = wb.SheetNames[0]
       const ws = wb.Sheets[wsname]
       const data = XLSX.utils.sheet_to_json(ws)
-      resolve(data)
+      
+      resolve(convertXLJson(data))
     }
     reader.onerror = (error) => {
       reject(error)
     }
   })
+}
+
+export const acceptedFileTypesList = [
+  'xlsx',
+  'xlsb',
+  'xlsm',
+  'xls',
+  'xml',
+  'csv',
+  'txt',
+  'ods',
+]
+
+export const acceptedFileTypesString = acceptedFileTypesList
+  .map((x) => '.' + x)
+  .join(',')
+
+export function convertXLJson(json) {
+  const cols = Object.keys(json[0])
+  const rows = json.map((row) => Object.values(row))
+  return { json, cols, rows }
 }
